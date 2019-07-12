c379381439140f3d5f79a2d9582e400b39457c33
Elvilius <vik.sergienko@mail.ru>
2019-07-12 15:01:39 +0300

    fixed code style

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env', 
-    {
-      targets: {
-        node: 'current',
-      },
-    }],
+    ['@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      }],
   ],
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import readlineSync from 'readline-sync'; 
+import readlineSync from 'readline-sync';
 
+// eslint-disable-next-line import/prefer-default-export
 export const askName = () => {
-    const name = readlineSync.question('May I have your name? ') || 'Guest';
-    console.log(`Hello, ${name}`);
-}
+  const name = readlineSync.question('May I have your name? ') || 'Guest';
+  console.log(`Hello, ${name}`);
+};
